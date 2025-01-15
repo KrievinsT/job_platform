3893a183e8473bd1c3bd71ad161d36c8f9e58c88
TTruble <blazingaustin@gmail.com>
2025-01-15 10:44:26 +0200

    testimonials section in about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -29,8 +29,47 @@ const About = () => {
             />
           </div>
 
+          <div className="about__missions-values-wrapper">
+            <div className="about__missions-values">
+              <div className="about__left-column">
+                <div className="about__section missions">
+                  <h2 className="about__section-title">Our missions</h2>
+                  <p className="about__section-text">
+                    Certain things catch your eye, but pursue only those that capture the heart. 
+                    We become what we think about. The person who says it cannot be done should
+                  </p>
+                </div>
+
+                <div className="about__section values">
+                  <h2 className="about__section-title">Our values</h2>
+                  <p className="about__section-text">
+                    Life is what happens to you while you're busy making other plans. 
+                    Build your own dreams, or someone else will hire you to build theirs.
+                  </p>
+                </div>
+              </div>
+
+              <div className="about__testimonial">
+                <p className="about__testimonial-text">
+                  I needed a change, to be able to make more money and have a life outside my job. 
+                  Work App helped me discover something better.
+                </p>
+                <div className="about__testimonial-author">
+                  <img 
+                    src="https://framerusercontent.com/images/1EtVEnelxTuAvPeZCWOZMnHKjI.jpg?scale-down-to=512" 
+                    alt="Naseema Al Morad"
+                    className="about__testimonial-image" 
+                  />
+                  <div className="about__testimonial-info">
+                    <h3 className="about__testimonial-name">Naseema Al Morad</h3>
+                    <p className="about__testimonial-position">Developer at Dropbox Inc.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
       <Footer />
     </>
   );
